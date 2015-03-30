8fc7fb424e3ca03de29a54ac70c3a7af710e5212
David Rutsch <daverutsch@gmail.com>
2015-03-30 18:35:28 -0500

    Update InputToMacAddressDirective.js

--- InputToMacAddressDirective.js
+++ InputToMacAddressDirective.js
@@ -6,26 +6,27 @@ app.directive('macAddress', function () {
 
             if (!ctrl) return;
 
-            var hexadecimalParse = function (value) {
+            var macAddressParse = function (value) {
                 return value.toUpperCase();
             }
 
-            var hexadecimalFormat = function (value) {
-
+            var macAddressFormat = function (value) {
+                
                 if (!value) return undefined;
-
+                
                 var numbers = value.replace(/-/g, "");
 
                 if (value.length % 3 === 0) {
+                    
                     return numbers.replace(/([0-9A-Za-z]{2})/g, "$1-");
                 }
             }
 
-            ctrl.$parsers.push(hexadecimalParse);
-            ctrl.$formatters.push(hexadecimalFormat);
+            ctrl.$parsers.push(macAddressParse);
+            ctrl.$formatters.push(macAddressFormat);
 
             ele.on('input', function () {
-                var value = hexadecimalFormat(ele.val());
+                var value = macAddressFormat(ele.val());
 
                 if (value !== undefined) {
                     ctrl.$setViewValue(value);
