9e68c76ef4589404a5c3a78df04cd0ecd8c526fe
Dan Morphis <dan@milkcarton.com>
2016-01-26 15:13:57 -0900

    - Format pasted values  - Convert Cisco MAC notation (aabb.ccdd.eeff) to : notation  - Convert '-' notation to ':' notation  - Add the symbol to use as a attr

--- InputToMacAddressDirective.js
+++ InputToMacAddressDirective.js
@@ -3,6 +3,7 @@ app.directive('macAddress', function () {
         restrict: "A",
         require: "ngModel",
         link: function (scope, ele, attr, ctrl) {
+            var symbol = attr.symbol ? attr.symbol : '-';
 
             if (!ctrl) {
                 return;
@@ -10,29 +11,37 @@ app.directive('macAddress', function () {
 
             var macAddressParse = function (value) {
                 return value.toUpperCase();
-            }
+            };
 
             var macAddressFormat = function (value) {
-                
-                if (!value) return undefined;
-                
-                var numbers = value.replace(/-/g, "");
-
-                if (value.length % 3 === 0) {
-                    
-                    return numbers.replace(/([0-9A-Za-z]{2})/g, "$1-");
+                if (!value) {
+                    return undefined;
                 }
-            }
+
+                var numbers = value.replace(/[-\.:]/g, "");
+
+                if (numbers.length % 3 === 0) {
+                    var result = numbers.replace(/([0-9A-Za-z]{2})/g, "$1" + symbol);
+
+                    if (result.length === 18 && result[result.length - 1] === symbol) {
+                        result = result.slice(0, -1);
+                    }
+                    return result;
+                }
+            };
 
             ctrl.$parsers.push(macAddressParse);
             ctrl.$formatters.push(macAddressFormat);
 
-            ele.on('input', function () {
-                var value = macAddressFormat(ele.val());
+            window.elem = ele;
+            ele.on("keyup keydown change", function (e) {
+                if (e.keyCode !== 8) {
+                    var value = macAddressFormat(ele.val());
 
-                if (value !== undefined) {
-                    ctrl.$setViewValue(value);
-                    ctrl.$render();
+                    if (value !== undefined) {
+                        ctrl.$setViewValue(value);
+                        ctrl.$render();
+                    }
                 }
                 scope.$apply();
             });
